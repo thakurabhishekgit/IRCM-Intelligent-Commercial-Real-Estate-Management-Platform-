379085297dd2709c114db13f35885e073e4af919
Abhishek Singh <abhishek.thakur.mainacc@gmail.com>
2026-05-26 18:35:45 +0530

    added authresponse

--- DTOs/Auth/AuthResponseDto.cs
+++ DTOs/Auth/AuthResponseDto.cs
@@ -6,13 +6,13 @@ public class AuthResponseDto
     public required string Token { get; set; }
     public Guid Id { get; set; }
 
-    public string FullName { get; set; }
+    public string FullName { get; set; } = string.Empty;
 
-    public string Email { get; set; }
+    public string Email { get; set; } = string.Empty;
 
-    public string PhoneNumber { get; set; }
+    public string PhoneNumber { get; set; } = string.Empty;
 
-    public string Role { get; set; }
+    public string Role { get; set; }    = string.Empty;
 
     public DateTime CreatedAt { get; set; }
 }
